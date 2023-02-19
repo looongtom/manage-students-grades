725aabcccbd9b97845eb656b45122386b1355819
looongtom <tuandabigkhoai@gmail.com>
2023-02-19 16:48:07 +0700

    Delete HelloServlet (Default code)

--- src/main/java/com/example/quanlysv/Servlet/Add_MH.java
+++ src/main/java/com/example/quanlysv/Servlet/Add_MH.java
@@ -1,15 +1,14 @@
 package com.example.quanlysv.Servlet;
 
-import com.example.quanlysv.HelloServlet;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet("/Add_MH")
-public class Add_MH extends HelloServlet {
+public class Add_MH extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doPost(req, resp);
