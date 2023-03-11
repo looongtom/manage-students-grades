04fb175b4a6f2587fd0276f862a0696601039355
Lại Ngọc Sơn <107608752+LNSdev07@users.noreply.github.com>
2023-03-11 15:27:10 +0700

    update ddl sonln
    update ddl sonln

--- src/main/resources/DDL.sql
+++ src/main/resources/DDL.sql
@@ -5,8 +5,8 @@ use quanlysinhvien
 
 create table THANHPHAN
 (
- id_tp int not null,
- id_diem int,
+ id_tp nvarchar(25) not null,
+ id_diem nvarchar(25),
  chuyen_can int,
  bai_tap int,
  thi int,
@@ -16,38 +16,38 @@ alter table THANHPHAN add  primary key(id_tp);
 
 create table MONHOC
 (
- id_mh int not null,
+ id_mh nvarchar(25) not null,
  ten_mon_hoc nvarchar(100),
  tin_chi int,
- id_khoa int,
+ id_khoa nvarchar(25),
 );
 alter table MONHOC add primary key(id_mh);
 
 create table HOCKY
 (
- id_hk int not null,
- ten_mon_hoc int,
+ id_hk nvarchar(25) not null,
+ ten_hoc_ky int,
 );
 alter table HOCKY add primary key(id_hk);
 
 create table DIEM 
 (
- id_diem int not null,
+ id_diem nvarchar(25) not null,
  diem_cc int,
- die_bt int,
+ diem_bt int,
  diem_thi int,
  diem_kt int,
  diem_th int,
- id_gv int,
- id_mh int,
- id_sv int,
- id_hk int,
+ id_gv nvarchar(25),
+ id_mh nvarchar(25),
+ id_sv nvarchar(25),
+ id_hk nvarchar(25),
 );
 alter table DIEM add primary key(id_diem);
 
 create table SINHVIEN
 (
- id_sv int not null,
+ id_sv nvarchar(25) not null,
  ten_sv nvarchar(50),
  email_sv nvarchar(80),
  dob_sv varchar(20),
@@ -59,40 +59,40 @@ alter table SINHVIEN add primary key(id_sv);
 
 create table LOP
 (
- id_lop int not null,
+ id_lop nvarchar(25) not null,
  ten_lop nvarchar(20),
- id_khoa int,
+ id_khoa nvarchar(25),
 );
 alter table LOP add primary key(id_lop);
 
 create table HOC
 (
- id_sv int not null,
- id_lop int,
+ id_sv nvarchar(25),
+ id_lop nvarchar(25),
 );
 
 create table KHOA
 (
- id_khoa int not null,
+ id_khoa nvarchar(25) not null,
  ten_khoa nvarchar(26),
 )
 alter table KHOA add primary key(id_khoa);
 
 create table GIANGVIEN
 (
- id_gv int not null,
- teb_gv nvarchar(50),
- sdt_fv varchar(14),
+ id_gv nvarchar(25) not null,
+ ten_gv nvarchar(50),
+ sdt_gv varchar(14),
  email_gv varchar(50),
  gender_gv int,
- id_khoa int,
+ id_khoa nvarchar(25),
 )
 alter table GIANGVIEN add primary key(id_gv);
 
 create table DAYHOC
 (
- id_gv int,
- id_mh int,
+ id_gv nvarchar(25),
+ id_mh nvarchar(25),
 );
 
 alter table DAYHOC add foreign key(id_gv) references GIANGVIEN(id_gv);
